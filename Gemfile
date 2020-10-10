7ab2a3c08d5ab282448ffbb3f25077cc4857c78f
suzumi <61306668+suzumi726@users.noreply.github.com>
2020-10-10 17:55:53 +0900

    ユーザー登録機能の作成

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 # 画像加工
 gem "refile-mini_magick"
+# ログイン
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
+
+  # get 'books/new'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # post 'books' => 'books#create'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
